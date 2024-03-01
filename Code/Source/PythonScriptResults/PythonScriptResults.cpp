9e32b432c3f69a12f1edcb4671aaaddcd1135cda
Artur Kamieniecki <artur.kamieniecki@robotec.ai>
2024-03-01 16:01:45 +0100

    Simplify querring for a variable
    Signed-off-by: Artur Kamieniecki <artur.kamieniecki@robotec.ai>

--- Code/Source/PythonScriptResults/PythonScriptResults.cpp
+++ Code/Source/PythonScriptResults/PythonScriptResults.cpp
@@ -5,7 +5,7 @@
 
 namespace AICore
 {
-    PythonScriptResults::PythonScriptResults()
+    void PythonScriptResults::ConnectToPythonConsole()
     {
         if (!m_isConnectedToPythonConsole)
         {
@@ -14,7 +14,7 @@ namespace AICore
         }
     }
 
-    PythonScriptResults::~PythonScriptResults()
+    void PythonScriptResults::DisconnectFromPythonConsole()
     {
         if (m_isConnectedToPythonConsole)
         {
@@ -25,25 +25,27 @@ namespace AICore
 
     AZ::Outcome<AZStd::string, AZStd::string> PythonScriptResults::GetVariableAsString(AZStd::string variable)
     {
-        m_readMessageAsVariable = true;
+        ConnectToPythonConsole();
+
         AzToolsFramework::EditorPythonRunnerRequestBus::Broadcast(
             &AzToolsFramework::EditorPythonRunnerRequestBus::Events::ExecuteByString,
             AZStd::string::format("print(str(%s))", variable.c_str()),
             false);
-        m_readMessageAsVariable = false;
+
+        DisconnectFromPythonConsole();
 
         AZStd::string result = "";
 
-        for (auto& message : m_argumentMessages)
+        for (auto& message : m_pythonMessages)
         {
             result += AZStd::string(message.data());
             result += AZStd::string("\n");
         }
-        m_argumentMessages.clear();
+        m_pythonMessages.clear();
 
-        if (m_error_or_exception)
+        if (m_errorOrException)
         {
-            m_error_or_exception = false;
+            m_errorOrException = false;
             return AZ::Failure(result);
         }
         return AZ::Success(result);
@@ -51,142 +53,17 @@ namespace AICore
 
     void PythonScriptResults::OnTraceMessage(AZStd::string_view message)
     {
-        if (!m_readMessageAsVariable)
-        {
-            m_traceMessages.push_back(message);
-        }
-        else
-        {
-            m_argumentMessages.push_back(message);
-        }
+        m_pythonMessages.push_back(message);
     }
 
     void PythonScriptResults::OnErrorMessage(AZStd::string_view message)
     {
-        if (!m_readMessageAsVariable)
-        {
-            m_errorMessages.push_back(message);
-        }
-        else
-        {
-            m_error_or_exception = true;
-            m_argumentMessages.push_back(message);
-        }
+        m_errorOrException = true;
+        m_pythonMessages.push_back(message);
     }
 
     void PythonScriptResults::OnExceptionMessage(AZStd::string_view message)
     {
-        if (!m_readMessageAsVariable)
-        {
-            m_exceptionMessages.push_back(message);
-        }
-        else
-        {
-            m_error_or_exception = true;
-            m_argumentMessages.push_back(message);
-        }
+        OnErrorMessage(message);
     }
-
-    unsigned long long PythonScriptResults::GetLinesSize(const AZStd::vector<AZStd::string_view>& lines)
-    {
-        return lines.size();
-    }
-    AZStd::string_view PythonScriptResults::GetLastMessage(const AZStd::vector<AZStd::string_view>& lines)
-    {
-        if (lines.empty())
-        {
-            return "";
-        }
-        return lines.back();
-    }
-    AZStd::string_view PythonScriptResults::GetLastUnreadMessage(AZStd::vector<AZStd::string_view>& lines, unsigned long long& readIndex)
-    {
-        if (lines.empty())
-        {
-            return "";
-        }
-        if (readIndex >= lines.size())
-        {
-            return "";
-        }
-        return lines[readIndex++];
-    }
-    AZStd::string_view PythonScriptResults::GetMessage(const AZStd::vector<AZStd::string_view>& lines, unsigned long long index)
-    {
-        if (index >= lines.size())
-        {
-            return "";
-        }
-        return lines[index];
-    }
-    const AZStd::vector<AZStd::string_view>& PythonScriptResults::GetMessages(const AZStd::vector<AZStd::string_view>& lines)
-    {
-        return lines;
-    }
-
-    unsigned long long PythonScriptResults::GetTraceLinesSize() const
-    {
-        return PythonScriptResults::GetLinesSize(m_traceMessages);
-    }
-    unsigned long long PythonScriptResults::GetErrorLinesSize() const
-    {
-        return PythonScriptResults::GetLinesSize(m_errorMessages);
-    }
-    unsigned long long PythonScriptResults::GetExceptionLinesSize() const
-    {
-        return PythonScriptResults::GetLinesSize(m_exceptionMessages);
-    }
-
-    AZStd::string_view PythonScriptResults::GetLastTraceMessage() const
-    {
-        return PythonScriptResults::GetLastMessage(m_traceMessages);
-    }
-    AZStd::string_view PythonScriptResults::GetLastErrorMessage() const
-    {
-        return PythonScriptResults::GetLastMessage(m_errorMessages);
-    }
-    AZStd::string_view PythonScriptResults::GetLastExceptionMessage() const
-    {
-        return PythonScriptResults::GetLastMessage(m_exceptionMessages);
-    }
-
-    AZStd::string_view PythonScriptResults::GetLastUnreadTraceMessage()
-    {
-        return PythonScriptResults::GetLastUnreadMessage(m_traceMessages, m_traceMessagesReadIndex);
-    }
-    AZStd::string_view PythonScriptResults::GetLastUnreadErrorMessage()
-    {
-        return PythonScriptResults::GetLastUnreadMessage(m_errorMessages, m_errorMessagesReadIndex);
-    }
-    AZStd::string_view PythonScriptResults::GetLastUnreadExceptionMessage()
-    {
-        return PythonScriptResults::GetLastUnreadMessage(m_exceptionMessages, m_exceptionMessagesReadIndex);
-    }
-
-    AZStd::string_view PythonScriptResults::GetTraceMessage(long long index) const
-    {
-        return PythonScriptResults::GetMessage(m_traceMessages, index);
-    }
-    AZStd::string_view PythonScriptResults::GetErrorMessage(long long index) const
-    {
-        return PythonScriptResults::GetMessage(m_errorMessages, index);
-    }
-    AZStd::string_view PythonScriptResults::GetExceptionMessage(long long index) const
-    {
-        return PythonScriptResults::GetMessage(m_exceptionMessages, index);
-    }
-
-    const AZStd::vector<AZStd::string_view>& PythonScriptResults::GetTraceMessages() const
-    {
-        return PythonScriptResults::GetMessages(m_traceMessages);
-    }
-    const AZStd::vector<AZStd::string_view>& PythonScriptResults::GetErrorMessages() const
-    {
-        return PythonScriptResults::GetMessages(m_errorMessages);
-    }
-    const AZStd::vector<AZStd::string_view>& PythonScriptResults::GetExceptionMessages() const
-    {
-        return PythonScriptResults::GetMessages(m_exceptionMessages);
-    }
-
 } // namespace AICore
--- Code/Source/PythonScriptResults/PythonScriptResults.h
+++ Code/Source/PythonScriptResults/PythonScriptResults.h
@@ -15,35 +15,10 @@ namespace AICore
     public:
         AZ_RTTI(PythonScriptResults, "{73c597bf-52e0-4255-ae6d-6a5182dec5f8}");
 
-        PythonScriptResults();
-        ~PythonScriptResults();
+        PythonScriptResults() = default;
+        ~PythonScriptResults() = default;
 
         AZ::Outcome<AZStd::string, AZStd::string> GetVariableAsString(AZStd::string variable);
-
-        unsigned long long GetTraceLinesSize() const;
-        unsigned long long GetErrorLinesSize() const;
-        unsigned long long GetExceptionLinesSize() const;
-
-        AZStd::string_view GetLastTraceMessage() const;
-        AZStd::string_view GetLastErrorMessage() const;
-        AZStd::string_view GetLastExceptionMessage() const;
-
-        AZStd::string_view GetLastUnreadTraceMessage();
-        AZStd::string_view GetLastUnreadErrorMessage();
-        AZStd::string_view GetLastUnreadExceptionMessage();
-
-        AZStd::string_view GetTraceMessage(long long index) const;
-        AZStd::string_view GetErrorMessage(long long index) const;
-        AZStd::string_view GetExceptionMessage(long long index) const;
-
-        const AZStd::vector<AZStd::string_view>& GetTraceMessages() const;
-        const AZStd::vector<AZStd::string_view>& GetErrorMessages() const;
-        const AZStd::vector<AZStd::string_view>& GetExceptionMessages() const;
-
-        void ClearTraceMessages();
-        void ClearErrorMessages();
-        void ClearExceptionMessages();
-        void ClearAllMessages();
 
     private:
         //////////////////////////////////////////////////////////////////////////
@@ -53,22 +28,8 @@ namespace AICore
         void OnExceptionMessage(AZStd::string_view message) override;
         //////////////////////////////////////////////////////////////////////////
 
-        static unsigned long long GetLinesSize(const AZStd::vector<AZStd::string_view>& lines);
-        static AZStd::string_view GetLastMessage(const AZStd::vector<AZStd::string_view>& lines);
-        static AZStd::string_view GetLastUnreadMessage(AZStd::vector<AZStd::string_view>& lines, unsigned long long& readIndex);
-        static AZStd::string_view GetMessage(const AZStd::vector<AZStd::string_view>& lines, unsigned long long index);
-        static const AZStd::vector<AZStd::string_view>& GetMessages(const AZStd::vector<AZStd::string_view>& lines);
-
-        AZStd::vector<AZStd::string_view> m_traceMessages;
-        unsigned long long m_traceMessagesReadIndex = 0;
-        AZStd::vector<AZStd::string_view> m_errorMessages;
-        unsigned long long m_errorMessagesReadIndex = 0;
-        AZStd::vector<AZStd::string_view> m_exceptionMessages;
-        unsigned long long m_exceptionMessagesReadIndex = 0;
-        AZStd::vector<AZStd::string_view> m_argumentMessages;
-
-        bool m_readMessageAsVariable = false;
-        bool m_error_or_exception = false;
+        AZStd::vector<AZStd::string_view> m_pythonMessages;
+        bool m_errorOrException = false;
 
         bool m_isConnectedToPythonConsole = false;
         void ConnectToPythonConsole();
