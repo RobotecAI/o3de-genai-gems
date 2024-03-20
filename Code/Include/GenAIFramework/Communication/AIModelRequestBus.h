25dbe07b0324285a938688cadc45d6389219a165
Jan Hanca <jan.hanca@robotec.ai>
2024-03-20 17:40:21 +0100

    change AIModelRequestBus output type
    Signed-off-by: Jan Hanca <jan.hanca@robotec.ai>

--- Code/Include/GenAIFramework/Communication/AIModelRequestBus.h
+++ Code/Include/GenAIFramework/Communication/AIModelRequestBus.h
@@ -17,14 +17,13 @@
 
 namespace GenAIFramework
 {
-    using ModelResponse = AZ::Outcome<AZStd::string, AZStd::string>; //! The type answer from the model
-    using ModelAPIRequest = AZStd::string; //! The type of request to send to the model
-    using ModelAPIResponse = AZ::Outcome<AZStd::string, AZStd::string>; //! The type of response from the model
+    using ModelResponse = AZ::Outcome<AZStd::string, AZStd::string>; //!< The type answer from the model
+    using ModelAPIRequest = AZStd::string; //!< The type of request to send to the model
+    using ModelAPIResponse = AZ::Outcome<AZStd::string, AZStd::string>; //!< The type of response from the model
 
     class AIModelRequest : public AZ::ComponentBus
     {
     public:
-
         AZ_RTTI(AIModelRequest, "{8c140e10-e3a2-478b-9886-c7c24eb16816}", AZ::ComponentBus);
 
         static const AZ::EBusHandlerPolicy HandlerPolicy = AZ::EBusHandlerPolicy::Single;
@@ -40,7 +39,7 @@ namespace GenAIFramework
         //! Extract the actual response from the model API response
         //! @param modelAPIResponse The response from the model, can be a JSON string that API around the model produced.
         //! @return The response from the model, or an error message if the response could not be extracted.
-        virtual AZ::Outcome<AZStd::string, AZStd::string> ExtractResult(const ModelAPIResponse& modelAPIResponse) = 0;
+        virtual ModelAPIResponse ExtractResult(const ModelAPIResponse& modelAPIResponse) = 0;
 
         //! Optional method to set custom model parameters (e.g. stop sequence)
         //! @param parameterName The name of the parameter to set.
