39c6237ed43aede52cbb2af81efe213d088ba793
Artur Kamieniecki <artur.kamieniecki@robotec.ai>
2024-04-17 14:02:10 +0200

    Add empty gem files
    Signed-off-by: Artur Kamieniecki <artur.kamieniecki@robotec.ai>

---
+++ Gems/GenAIBedrockVendorBundle/Code/Platform/Windows/genaibedrockvendorbundle_private_files.cmake
@@ -0,0 +1,8 @@
+
+# Platform specific files for Windows
+# i.e. ../Source/Windows/GenAIBedrockVendorBundleWindows.cpp
+#      ../Source/Windows/GenAIBedrockVendorBundleWindows.h
+#      ../Include/Windows/GenAIBedrockVendorBundleWindows.h
+
+set(FILES
+)
---
+++ Gems/GenAIBedrockVendorBundle/Code/Platform/iOS/genaibedrockvendorbundle_shared_files.cmake
@@ -0,0 +1,8 @@
+
+# Platform specific files for iOS
+# i.e. ../Source/iOS/GenAIBedrockVendorBundleiOS.cpp
+#      ../Source/iOS/GenAIBedrockVendorBundleiOS.h
+#      ../Include/iOS/GenAIBedrockVendorBundleiOS.h
+
+set(FILES
+)
